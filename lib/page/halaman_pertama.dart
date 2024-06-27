2fef771422e6ac4cdd6b9bb5107bca1e6dee8b79
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 18:31:51 +0700

    membuat class halaman pertama

---
+++ lib/page/halaman_pertama.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HalamanPertama extends StatelessWidget {
+  const HalamanPertama({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
