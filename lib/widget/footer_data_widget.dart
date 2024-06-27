d87ca76fcb0e340747f2d89c294c39b5ac7a429d
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 19:36:31 +0700

    membuat class footerdatawidget

---
+++ lib/widget/footer_data_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FooterDataWidget extends StatelessWidget {
+  const FooterDataWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
