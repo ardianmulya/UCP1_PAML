bcba349f8158f1a76f0d8f1ca93c8db809b02933
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 21:02:16 +0700

    membuat halaman ketiga

---
+++ lib/page/halaman_ketiga.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HalamanKetiga extends StatelessWidget {
+  const HalamanKetiga({super.key, required String nama, required String notelp, required String makanan, required String minuman});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
