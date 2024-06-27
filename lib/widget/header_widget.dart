4e4c791c874ea5191a35849e976eba83fba3f4c8
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 18:45:53 +0700

    menambah logo warmindo

--- lib/widget/header_widget.dart
+++ lib/widget/header_widget.dart
@@ -14,6 +14,15 @@ class HeaderWidget extends StatelessWidget {
           "Warmindo Mang Ujo",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
         ),
+        const SizedBox(
+          height: 25,
+        ),
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+            "https://ugc.production.linktr.ee/4055b2bd-17b1-4ed5-aff9-9de4e98d6556_Logo-Warmindo-Mang-Ujo-1.0.png",
+          ),
+          radius: 70,
+        )
       ]
     );
   }
