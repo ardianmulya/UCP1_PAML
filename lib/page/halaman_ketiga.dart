cc837ad0cce48aee41d35aa3d30953fa9025ff92
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 21:03:55 +0700

    menambahkan variabel global

--- lib/page/halaman_ketiga.dart
+++ lib/page/halaman_ketiga.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HalamanKetiga extends StatelessWidget {
-  const HalamanKetiga({super.key, required String nama, required String notelp, required String makanan, required String minuman});
+  const HalamanKetiga({super.key, required this.alamat, required this.nama, required this.notelp, required this.makanan, required this.minuman});
 
+  final String nama;
+  final String notelp;
+  final String alamat;
+  final String makanan;
+  final String minuman;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
