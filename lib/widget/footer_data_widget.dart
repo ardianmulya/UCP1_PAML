cb9c9b2b21688e7aa1de40766adeef6acc27d0dc
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 19:38:24 +0700

    membuat button

--- lib/widget/footer_data_widget.dart
+++ lib/widget/footer_data_widget.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class FooterDataWidget extends StatelessWidget {
-  const FooterDataWidget({super.key});
-
+  const FooterDataWidget({super.key, required this.onPressed});
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 25,
+        ),
+        ElevatedButton(
+          onPressed: onPressed,
+          child: const Text("Selanjutnya"),
+        ),
+      ],
+    );
   }
 }
