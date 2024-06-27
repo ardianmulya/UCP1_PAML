9b04a2150580d330d189c3455ca4c0e73b9c884f
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 21:01:40 +0700

    menambah variabel alamat

--- lib/data/pesan.dart
+++ lib/data/pesan.dart
@@ -1,11 +1,13 @@
 class pesan {
   final String nama;
   final String notelp;
+  final String alamat;
   final String makanan;
   final String minuman;
   pesan({
     required this.nama,
     required this.notelp,
+    required this.alamat,
     required this.makanan,
     required this.minuman,
   });
