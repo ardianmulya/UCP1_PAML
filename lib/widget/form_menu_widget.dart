6e8ab5153ec8b6eb06c88ce8012d4d1cdc39d6ec
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 20:48:02 +0700

    membuat variabel global dan menambah controller

--- lib/widget/form_menu_widget.dart
+++ lib/widget/form_menu_widget.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
-class FormMenuWidget extends StatelessWidget {
-  const FormMenuWidget({super.key});
+class FormMenuWidget extends StatefulWidget {
+  const FormMenuWidget({super.key, required this.formmenuKey, required this.MakananController, required this.MinumanController});
 
+  final GlobalKey<FormState> formmenuKey;
+  final TextEditingController MakananController;
+  final TextEditingController MinumanController;
+
+  @override
+  State<FormMenuWidget> createState() => _FormMenuWidgetState();
+}
+
+class _FormMenuWidgetState extends State<FormMenuWidget> {
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: widget.formmenuKey,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             TextFormField(
               keyboardType: TextInputType.name,
+              controller: widget.MakananController,
               decoration: const InputDecoration(
                 labelText: "Makanan",
                 hintText: "Masukkan Makanan Kamu",
@@ -30,6 +41,7 @@ class FormMenuWidget extends StatelessWidget {
             ),
             TextFormField(
               keyboardType: TextInputType.name,
+              controller: widget.MinumanController,
               decoration: const InputDecoration(
                 labelText: "Minuman",
                 hintText: "Masukkan Minuman Kamu",
