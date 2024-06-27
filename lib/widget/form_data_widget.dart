481c89708ef48adbdc0fef85473bc1b8b591fb3b
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 19:01:17 +0700

    membuat textformfiled no telepon beserta validasi

--- lib/widget/form_data_widget.dart
+++ lib/widget/form_data_widget.dart
@@ -27,11 +27,34 @@ class _FormDataWidgetState extends State<FormDataWidget> {
                 validator: (value) {
                   bool valid = RegExp(r'[a-zA-Z]').hasMatch(value!);
                   if (value!.isEmpty) {
-                    return "Form ini wajib di isi!";
+                    return "Nama wajib di isi!";
                   } else if (!valid) {
                     return "Nama tidak boleh berisi angka!";
                   } else if (value.length <= 2) {
                     return "Nama Terlalu Pendek!";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "No Telepon",
+                  hintText: "Masukkan No Telp Kamu",
+                  prefixIcon: Icon(Icons.phone),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                   bool valid = RegExp(r'[0-9]+$').hasMatch(value!);
+                  if (value!.isEmpty) {
+                    return "No Telepon wajib di isi!";
+                  } else if (!valid) {
+                    return "No Telepon tidak Valid!";
+                  } else if (value.length <= 2) {
+                    return "No Telepon Terlalu Pendek!";
                   }
                   return null;
                 },
