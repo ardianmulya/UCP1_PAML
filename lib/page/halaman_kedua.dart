524190f2c2a445b6fa8c6d7ae1006b18bd61399b
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 20:15:40 +0700

    membuat class halaman kedua dan membuat variabel global

---
+++ lib/page/halaman_kedua.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HalamanKedua extends StatelessWidget {
+  const HalamanKedua({super.key, required this.nama, required this.notelp, required this.alamat});
+
+  final String nama;
+  final String notelp;
+  final String alamat;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
