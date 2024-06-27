3aed59f0a58afc0eb0127eb4c7e9e3c1d20c40f1
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 20:54:02 +0700

    membuat class pesan

---
+++ lib/data/pesan.dart
@@ -0,0 +1,12 @@
+class pesan {
+  final String nama;
+  final String notelp;
+  final String makanan;
+  final String minuman;
+  pesan({
+    required this.nama,
+    required this.notelp,
+    required this.makanan,
+    required this.minuman,
+  });
+}
