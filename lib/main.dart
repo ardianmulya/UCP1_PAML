afa6b1de4e3be83b43341d8ceebccb8193241666
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 21:31:26 +0700

    memanggil halaman pertama

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ucppaml/page/halaman_kedua.dart';
+import 'package:ucppaml/page/halaman_ketiga.dart';
 import 'package:ucppaml/page/halaman_pertama.dart';
+import 'package:ucppaml/widget/form_data_widget.dart';
+import 'package:ucppaml/widget/form_menu_widget.dart';
+import 'package:ucppaml/widget/header_data_widget.dart';
+import 'package:ucppaml/widget/header_menu_widget.dart';
 
 void main() {
   runApp(const MyApp());
