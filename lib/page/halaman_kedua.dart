4f82ad054f77ed87571baf6722d71d48b86cb6f6
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 20:40:21 +0700

    memanggil HeaderMenuWidget

--- lib/page/halaman_kedua.dart
+++ lib/page/halaman_kedua.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:ucppaml/widget/header_menu_widget.dart';
 
 class HalamanKedua extends StatelessWidget {
-  const HalamanKedua({super.key, required this.nama, required this.notelp, required this.alamat});
+  const HalamanKedua(
+      {super.key,
+      required this.nama,
+      required this.notelp,
+      required this.alamat});
 
   final String nama;
   final String notelp;
   final String alamat;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            HeaderMenuWidget(nama: nama, notelp: notelp, alamat: alamat),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
