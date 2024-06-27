0e094666bd55e364c14969e5ec3ba169e3aa7b5d
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 19:41:20 +0700

    memanggil HeaderDataWidget

--- lib/page/halaman_pertama.dart
+++ lib/page/halaman_pertama.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:ucppaml/widget/header_data_widget.dart';
 
 class HalamanPertama extends StatelessWidget {
   const HalamanPertama({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          HeaderDataWidget(),
+        ],
+      ),
+    );
   }
 }
