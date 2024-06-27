18a4c610a40917ea3deeb9ea34c399a6bc214c52
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 19:52:03 +0700

    memanggil footerdatawidget dan menambhkan fungsi pindah halaman

--- lib/page/halaman_pertama.dart
+++ lib/page/halaman_pertama.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ucppaml/page/halaman_kedua.dart';
+import 'package:ucppaml/widget/footer_data_widget.dart';
 import 'package:ucppaml/widget/form_data_widget.dart';
 import 'package:ucppaml/widget/header_data_widget.dart';
 
@@ -21,6 +23,24 @@ class HalamanPertama extends StatelessWidget {
               NamaControoller: nama,
               NotelpController: notelp,
               AlamatController: alamat),
+          FooterDataWidget(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HalamanKedua(
+                        nama: nama.text,
+                        notelp: notelp.text,
+                        alamat: alamat.text,
+                      ),
+                    ),
+                    (route) => false);
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text("Berhasil")));
+              }
+            },
+          ),
         ],
       ),
     );
