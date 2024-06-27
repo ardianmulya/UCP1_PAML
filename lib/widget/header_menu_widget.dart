28f4abb92fdc1f1e029db7db0ede2adeaa68d77e
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 20:31:17 +0700

    menambahkan card dan text

--- lib/widget/header_menu_widget.dart
+++ lib/widget/header_menu_widget.dart
@@ -5,6 +5,59 @@ class HeaderMenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Card(
+        elevation: 4,
+        child: SizedBox(
+          height: 140,
+          width: 1250,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Selamat Datang, nama',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "No Telpon : notelp",
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Alamat : alamat",
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "Tulis Pesanananmu Disini",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
