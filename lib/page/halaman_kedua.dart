251ba04ed3a7786c97d56a22ffde7117f14950d4
ardianmulya <ardian.mulya7@gmail.com>
2024-06-27 21:01:30 +0700

    memanggil footermenuwidget dan menambah fungsi pindah halaman

--- lib/page/halaman_kedua.dart
+++ lib/page/halaman_kedua.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ucppaml/data/pesan.dart';
+import 'package:ucppaml/page/halaman_ketiga.dart';
+import 'package:ucppaml/widget/footer_menu_widget.dart';
 import 'package:ucppaml/widget/form_menu_widget.dart';
 import 'package:ucppaml/widget/header_menu_widget.dart';
 
@@ -24,6 +27,20 @@ class HalamanKedua extends StatelessWidget {
           children: [
             HeaderMenuWidget(nama: nama, notelp: notelp, alamat: alamat),
             FormMenuWidget(formmenuKey: formmenuKey, MakananController: namaMakanan, MinumanController: namaMinuman),
+            FooterMenuWidget(onPressed: (){
+              if (formmenuKey.currentState!.validate()) {
+                  formmenuKey.currentState!.save();
+                  pesan pesanBaru = pesan(nama: nama, notelp: notelp,alamat: alamat, makanan: namaMakanan.text, minuman: namaMinuman.text);
+                  Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                          builder: (pesanBaru) => HalamanKetiga(
+                                nama: nama,
+                                notelp: notelp,
+                                makanan: namaMakanan.text,
+                                minuman: namaMinuman.text,
+                              )),
+                      (route) => false);
+                  }
+            })
           ],
         ),
       ),
